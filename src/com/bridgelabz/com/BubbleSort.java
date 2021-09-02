869444a9e3299489418ad2c36e8fbd80de3ea561
namancg <naman.c@perfios.com>
2021-09-02 23:32:31 +0530

    REFACTORED: implemented bubble sort using generics

--- src/com/bridgelabz/com/BubbleSort.java
+++ src/com/bridgelabz/com/BubbleSort.java
@@ -4,25 +4,27 @@ public class BubbleSort {
 
 	public static void main(String args[])
     {
-        int arr[] = {19,66,44,9,54,43,24,75,36,55,34};
+        Integer[] arr = {19,66,44,9,54,43,24,75,36,55,34};
         bubbleSort(arr);
-        System.out.println("Sorted array");
-        for (int i=0;i<arr.length;i++)
-        {
-        	System.out.println(arr[i]);
-        }
+        String[] stringArray= {"HELLO","WELCOME","TO","PERFIOS"};
+        bubbleSort(stringArray);
+       
     }
-	public static int[] bubbleSort(int arr[])
+	public static<T extends Comparable<T>> void bubbleSort(T[] arr)
     {
         int n = arr.length;
         for (int i = 0; i < n-1; i++)
-            for (int j = 0; j < n-i-1; j++)
-                if (arr[j] > arr[j+1])
+            for (int j = i; j < n; j++)
+                if (arr[i].compareTo(arr[j]) < 0)
                 {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    T temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
-        return arr;
+        System.out.println("AFTER SORTING");
+        for( int i=0;i<arr.length;i++)
+        {
+        	System.out.println(arr[i]);
+        }
     }
 }
